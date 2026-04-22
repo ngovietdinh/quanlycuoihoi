6d6593fa07094deab6cc78ab3307e638346109bc
ngovietdinh <gsnvbu@gmail.com>
2026-04-22 18:14:53 +0700

    update ui v3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 import './globals.css'
 export const metadata: Metadata = {
-  title: { default:'Lễ Ăn Hỏi', template:'%s | Lễ Ăn Hỏi' },
-  description:'Hệ thống quản lý lễ ăn hỏi toàn diện',
+  title: { default:'Quán lý sự kiện', template:'%s | Quán lý sự kiện' },
+  description:'Hệ thống Quán lý sự kiện toàn diện',
 }
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
